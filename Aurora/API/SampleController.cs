5d0f1f709131d12d4dc8de5dda1a6def3cad5f8f
warlord1609 <bhargavkrishna16@gmail.com>
2013-08-19 21:04:54 +0530

    Fixes and Updates
    Updated errors in project loading due to versioning errors. Update your
    projects using Package Manger Console to the latest versions.

---
+++ Aurora/API/SampleController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Aurora.Models.Primitives;
+using Aurora.Entity;
+
+namespace Aurora.API
+{
+    public class SampleController : ApiController
+    {
+        private AuroraContext db = new AuroraContext();
+
+        // GET api/Sample
+        public IEnumerable<Company> GetCompanies()
+        {
+            return db.Companies.AsEnumerable();
+        }
+
+        // GET api/Sample/5
+        public Company GetCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return company;
+        }
+
+        // PUT api/Sample/5
+        public HttpResponseMessage PutCompany(int id, Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != company.CompanyId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Sample
+        public HttpResponseMessage PostCompany(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Companies.Add(company);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, company);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = company.CompanyId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Sample/5
+        public HttpResponseMessage DeleteCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Companies.Remove(company);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, company);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
